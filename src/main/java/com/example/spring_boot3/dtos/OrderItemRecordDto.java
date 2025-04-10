1d7ab586ce6f132acc968a26f2003034029c3d36
dmarostega@gmail.com <dmarostega@gmail.com>
2025-04-10 10:55:33 -0300

    feat(order): rotas list, update, delete, getById; refactor(cliente, order item): ajuste código, correções Annotations

--- src/main/java/com/example/spring_boot3/dtos/OrderItemRecordDto.java
+++ src/main/java/com/example/spring_boot3/dtos/OrderItemRecordDto.java
@@ -1,7 +1,8 @@
 package com.example.spring_boot3.dtos;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record OrderItemRecordDto(@NotNull String product_id, @NotBlank String order_id, @NotNull Integer quantity) {
+public record OrderItemRecordDto(@NotNull String product_id, @NotBlank String order_id, @Min(0) Integer quantity) {
 }
